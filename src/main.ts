a27e027767cd6325d051e297f291244894119a5f
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-04-28 17:20:09 +0500

    added

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   // Swagger konfiguratsiyasi
   const config = new DocumentBuilder()
     .setTitle('Scanner-POS')
