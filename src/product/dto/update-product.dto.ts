d8e449be2791dff942c8db1ba65da19f8269dc39
isomiddinovbriu-ui <isomiddinovbriu@gmail.com>
2026-01-06 12:18:52 +0500

    asd

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,31 +1,33 @@
 import { IsOptional, IsString, IsNumber, IsArray, IsIn } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class UpdateProductDto {
-  @IsOptional()
-  @IsString()
-  barcode?: string;
-
   @IsOptional()
   @IsString()
   name?: string;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   branch_id?: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   category_id?: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   price?: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   stock?: number;
 
   @IsOptional()
+  @Type(() => Number)
   @IsNumber()
   real_price?: number;
 
@@ -37,17 +39,4 @@ export class UpdateProductDto {
   @IsArray()
   @IsIn(['new', 'hit', 'sale'], { each: true })
   tegs?: ('new' | 'hit' | 'sale')[];
-
-  // Faqat qo‘shimcha qilish yoki o‘chirish uchun
-  @IsOptional()
-  @IsArray()
-  addImages?: any[];
-
-  @IsOptional()
-  @IsArray()
-  removeImages?: any[];
-
-  // ✅ DB update uchun ichki property
-  @IsOptional()
-  imageUrls?: string[];
 }
